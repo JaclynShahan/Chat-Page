f0e228c93d9b4665c741ffa61aae045231d281bf
Jaclyn <jaclyndshahan@gmail.com>
2020-01-16 17:00:04 -0600

    worked on login page

---
+++ src/components/Login/Login.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+import {Icon, Button} from 'antd'
+
+class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            user: {}
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <form>
+                    <img />
+                    <div>
+                        <label><Icon type="user"/></label>
+                        <input 
+                        placeholder="Username"
+                        />
+                        <label><Icon type="unlock" /></label>
+                        <input 
+                        placeholder="Password"
+                        />
+                    </div>
+                    <div>
+                        <Button>Login</Button>
+                        <span>OR</span>
+                        <Button>Create Account</Button>
+                    </div>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default Login
