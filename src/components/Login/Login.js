3a30ef5912a9d758f0b84100e125f0b95bbf26db
Jaclyn <jaclyndshahan@gmail.com>
2020-01-17 20:20:11 -0600

    created login reducer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,40 +1,39 @@
-import React, {Component} from 'react'
-import {Icon, Button} from 'antd'
+import React, { Component } from 'react'
+import { Icon, Button } from 'antd'
 
 class Login extends Component {
-    constructor() {
-        super()
-        this.state = {
-            loading: false,
-            user: {}
-        }
+  constructor () {
+    super()
+    this.state = {
+      loading: false,
+      user: {}
     }
+  }
 
-    render() {
-        return(
-            <div>
-                <form>
-                    <img />
-                    <div>
-                        <label><Icon type="user"/></label>
-                        <input 
-                        placeholder="Username"
-                        />
-                        <label><Icon type="unlock" /></label>
-                        <input 
-                        placeholder="Password"
-                        />
-                    </div>
-                    <div>
-                        <Button>Login</Button>
-                        <span>OR</span>
-                        <Button>Create Account</Button>
-                    </div>
-                </form>
-
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <form>
+          <img />
+          <div>
+            <label>
+              <Icon type='user' />
+            </label>
+            <input placeholder='Username' />
+            <label>
+              <Icon type='unlock' />
+            </label>
+            <input placeholder='Password' />
+          </div>
+          <div>
+            <Button>Login</Button>
+            <span>OR</span>
+            <Button>Create Account</Button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Login
