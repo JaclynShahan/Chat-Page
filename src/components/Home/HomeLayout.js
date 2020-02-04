7ff21e363ea3fe575818f186e19482e845cbdb0d
Jaclyn <jaclyndshahan@gmail.com>
2020-02-03 19:27:44 -0600

    creating home layout

---
+++ src/components/Home/HomeLayout.js
@@ -0,0 +1,20 @@
+import React, {Component} from React;
+
+
+class HomeLayout extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default HomeLayout
